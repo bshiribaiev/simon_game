c165eda9f92aca7dd43e19493984f87c87e293c3
Bekbol Shiribaiev <bekbolnyc@gmail.com>
2025-07-01 00:47:37 -0400

    complete

--- game.js
+++ game.js
@@ -2,10 +2,12 @@ const buttonColors = ["red", "blue", "green", "yellow"]
 let gamePattern = []
 let userPattern = []
 let level = 0
+let start = false
 
 $('.btn').click(handleClick)
 
 function nextSequence() {
+    userPattern = [];
     level++
     $('h1').text("Level " + level)
     let randomNumber = Math.floor(Math.random() * 4)
@@ -22,24 +24,45 @@ function playSound(color) {
 
 function animatePress(currentColor) {
     $('#' + currentColor).addClass('pressed')
-    setTimeout(function () {$('#' + currentColor).remove('pressed'), 100})
+    setTimeout(function () {$('#' + currentColor).removeClass('pressed')}, 100)
 }
 
 function handleClick() {
-    userColor = $(this).attr('id')
+    if (!start) 
+        return
+    let userColor = $(this).attr('id')
     userPattern.push(userColor)
     playSound(userColor)
     animatePress(userColor)
-    checkAnswer()
+    checkAnswer(userPattern.length - 1) 
 }
 
 function handleKey() {
-    $('h1').text("Level " + level)
-    nextSequence()
+    if(!start) {
+        start = true
+        $('h1').text("Level " + level)
+        nextSequence()
+    }
+
 }
-$(document).one("keydown", handleKey)
+$(document).keydown(handleKey)
 
-function checkAnswer(color) {
-    if (color === 
+function checkAnswer(idx) {
+    if (userPattern[idx] === gamePattern[idx]) {
+        if (userPattern.length === gamePattern.length)
+            setTimeout(nextSequence, 1000)
+    }
+    else {
+        playSound('wrong')
+        $('body').addClass('game-over')
+        $('h1').text("Game Over, Press Any Key to Restart")
+        setTimeout(function() {$('body').removeClass('game-over')}, 100)
+        startOver()
+    }
+}
 
+function startOver() {
+    level = 0
+    gamePattern = []
+    start = false
 }
